09090ddca113d5608fb4432c7a02894bff0b4356
Juanma42 <juanmanueltabalv@gmail.com>
2021-08-11 09:34:05 +0200

    correcciones 2

--- calculadora/calculator pro juanma.js
+++ calculadora/calculator pro juanma.js
@@ -1,13 +1,9 @@
 //The 3 decimal function because I hate how convoluted it has to be and it and should be placed in a dark corner and never spoken of
-function tresDeci(num)
-{
-	if (num%1!=0)
-	{
-		num=parseFloat(num.toFixed(3));
+function tresDeci(num) {
+	if (num % 1 != 0) {
+		num = parseFloat(num.toFixed(3));
 		return num;
-	}
-	else
-	{
+	} else {
 		return num;
 	}
 }
@@ -15,146 +11,143 @@ function tresDeci(num)
 //The actual code
 
 //operators array
-let operadoresposibles=["+","-","/","*","raiz"]
-let numero1=0
-let numero2=0
-let operador=""
+let operadoresposibles = ["+", "-", "/", "*", "raiz"];
+let numero1 = 0;
+let numero2 = 0;
+let operador = "";
 
 //function for if the user is not very bright
-function nolisto()
-{
-	alert("Escribe un numero, pero con numeros, no con letras. \nVuelve a intentarlo anda.");	
+function nolisto() {
+	alert(
+		"Escribe un numero, pero con numeros, no con letras. \nVuelve a intentarlo anda."
+	);
 }
 
 //first number writing function, checks too
 
-function firstnum()
-{
-	numero1 = parseFloat(prompt('Primer número'));
-	if (Number.isNaN(numero1))
-	{
-		nolisto()
-		firstnum()
-	}
-	else
-	{
-			//Displaying info
-		alert("El primer numero es "+numero1);		
+function firstnum() {
+	numero1 = parseFloat(prompt("Primer número"));
+	if (Number.isNaN(numero1)) {
+		nolisto();
+		firstnum();
+	} else {
+		//Displaying info
+		alert("El primer numero es " + numero1);
 	}
 	return numero1;
 }
 
-
 //asking for operator and displaying
-function operadorcheck()
-{
-	operador=prompt("Escoge un operador de los siguientes: +, - , * , / , raiz. \nSi escoges raiz, el segundo numero será su índice.");
+function operadorcheck() {
+	operador = prompt(
+		"Escoge un operador de los siguientes: +, - , * , / , raiz. \nSi escoges raiz, el segundo numero será su índice."
+	);
 
-	if (operadoresposibles.includes(operador))
-	{
-		alert("El operador escogido es "+operador);
-	}
-	else
-	{
+	if (operadoresposibles.includes(operador)) {
+		alert("El operador escogido es " + operador);
+	} else {
 		alert("No es un comando válido. Por favor, vuelve a intentarlo.");
 		operadorcheck();
 	}
-	return operador
+	return operador;
 }
 
 //asking for the second and displaying
-function secondnum()
-{
-	numero2 = parseFloat(prompt('Segundo numero'));
+function secondnum() {
+	numero2 = parseFloat(prompt("Segundo numero"));
 
-	if (Number.isNaN(numero2))
-	{
-		nolisto()
-		secondnum()
-	}
-	else
-	{
+	if (Number.isNaN(numero2)) {
+		nolisto();
+		secondnum();
+	} else {
 		//Displaying info
-		alert("El segundo numero es "+numero2);		
+		alert("El segundo numero es " + numero2);
 	}
 	return numero2;
 }
 
 //the operations
 
-function suma(num1,num2)
-{
-	return tresDeci(num1+num2);
+function suma(num1, num2) {
+	return tresDeci(num1 + num2);
 }
 
-function resta(num1,num2)
-{
-	return tresDeci(num1-num2);
+function resta(num1, num2) {
+	return tresDeci(num1 - num2);
 }
 
-function division(num1,num2)
-{
-	return tresDeci((num1)/(num2));
+function division(num1, num2) {
+	return tresDeci(num1 / num2);
 }
 
-function multiplicacion(num1,num2)
-{
-	return tresDeci(num1*num2);
+function multiplicacion(num1, num2) {
+	return tresDeci(num1 * num2);
 }
 
-function raiz(num1,num2)
-{
-	return num1**(1/num2);
+function raiz(num1, num2) {
+	return num1 ** (1 / num2);
 }
 
-function otravez()
-{
-	if (window.confirm("¿Quieres hacer otra operación?"))
-	{
-		location.reload()
+function otravez() {
+	if (window.confirm("¿Quieres hacer otra operación?")) {
+		location.reload();
 	}
 }
 
-
-
-function calculadorapro()
-{
+function calculadorapro() {
 	//Instructions
-	alert("Instrucciones: \n1-Introduce un numero\n2-Escoge un operador de los siguientes: +, - , * , / , raiz. Si escoges raiz, el segundo numero será su índice.\n3-Introduce otro numero\nTe devuelvo un resultado.");
-	numero1=firstnum()
-	operador=operadorcheck()
-	numero2=secondnum()
+	alert(
+		"Instrucciones: \n1-Introduce un numero\n2-Escoge un operador de los siguientes: +, - , * , / , raiz. Si escoges raiz, el segundo numero será su índice.\n3-Introduce otro numero\nTe devuelvo un resultado."
+	);
+	numero1 = firstnum();
+	operador = operadorcheck();
+	numero2 = secondnum();
 
-	switch(operador)
-	
-
-	{
+	switch (operador) {
 		case "+":
-		alert(`${numero1} \+ ${numero2} \= ${suma(numero1,numero2)}`);
-		console.log((`${numero1} \+ ${numero2} \= ${suma(numero1,numero2)}`));
-		break;
-	
+			alert(`${numero1} \+ ${numero2} \= ${suma(numero1, numero2)}`);
+			console.log(
+				`${numero1} \+ ${numero2} \= ${suma(numero1, numero2)}`
+			);
+			break;
+
 		case "-":
-		alert(`${numero1} \- ${numero2} \= ${resta(numero1,numero2)}`);
-		console.log((`${numero1} \- ${numero2} \= ${resta(numero1,numero2)}`));
-		break;
-		
+			alert(`${numero1} \- ${numero2} \= ${resta(numero1, numero2)}`);
+			console.log(
+				`${numero1} \- ${numero2} \= ${resta(numero1, numero2)}`
+			);
+			break;
+
 		case "*":
-		alert(`${numero1} \* ${numero2} \= ${multiplicacion(numero1,numero2)}`);
-		console.log((`${numero1} \* ${numero2} \= ${multiplicacion(numero1,numero2)}`));
-		break;
+			alert(
+				`${numero1} \* ${numero2} \= ${multiplicacion(
+					numero1,
+					numero2
+				)}`
+			);
+			console.log(
+				`${numero1} \* ${numero2} \= ${multiplicacion(
+					numero1,
+					numero2
+				)}`
+			);
+			break;
 
 		case "/":
-		alert(`${numero1} \/ ${numero2} \= ${division(numero1,numero2)}`);
-		console.log((`${numero1} \/ ${numero2} \= ${division(numero1,numero2)}`));
-		break;
+			alert(`${numero1} \/ ${numero2} \= ${division(numero1, numero2)}`);
+			console.log(
+				`${numero1} \/ ${numero2} \= ${division(numero1, numero2)}`
+			);
+			break;
 
 		case "raiz":
-		alert(`Raiz ${numero2} de ${numero1} \= ${raiz(numero1,numero2)}`);
-		console.log((`Raiz ${numero2} de ${numero1} \= ${raiz(numero1,numero2)}`));
-		break;	
+			alert(`Raiz ${numero2} de ${numero1} \= ${raiz(numero1, numero2)}`);
+			console.log(
+				`Raiz ${numero2} de ${numero1} \= ${raiz(numero1, numero2)}`
+			);
+			break;
 	}
-	otravez()
+	otravez();
 }
 
-calculadorapro()
+calculadorapro();
--- skylab airlines/skylab airlines PRO.js
+++ skylab airlines/skylab airlines PRO.js
@@ -1,270 +1,286 @@
 //the BIG DATA
 
 let flights = [
+    { id: 00, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
 
-    { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+    { id: 01, to: "New York", from: "Barcelona", cost: 700, scale: false },
 
-    { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+    { id: 02, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
 
-    { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+    { id: 03, to: "Paris", from: "Barcelona", cost: 210, scale: false },
 
-    { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+    { id: 04, to: "Roma", from: "Barcelona", cost: 150, scale: false },
 
-    { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+    { id: 05, to: "London", from: "Madrid", cost: 200, scale: false },
 
-    { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
+    { id: 06, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
 
-    { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+    { id: 07, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
 
-    { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+    { id: 08, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
 
-    { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+    { id: 09, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
 
-    { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
-
-    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false } ];
+    { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
+];
 
 //asks for user and greets
 
-function userwelcome()
-{
-    let user=prompt("Hola. ¿Quién te toca ser hoy?");
-    if (user===""){user="Anónima"}
-    alert(`Bienvenida sea la persona ${user}, \nse le desea fortuna en sus futuros viajes.\n(Que se muestran en la consola)`)
+function userwelcome() {
+    let user = prompt("Hola. ¿Quién te toca ser hoy?");
+    if (user === "") {
+        user = "Anónima";
+    }
+    alert(
+        `Bienvenida sea la persona ${user}, \nse le desea fortuna en sus futuros viajes.\n(Que se muestran en la consola)`
+    );
     return user;
-};
+}
 
 //Returns a message depending on the scale parameter
-function escala(scale)
-{
-if (scale)
-{
-    return "hace escala."
+function escala(scale) {
+    if (scale) {
+        return "hace escala.";
+    } else {
+        return "no hace escala.";
+    }
 }
-else
-{
-    return "no hace escala."
-}
-};
 
 //iterates through the array showing the sentence with each flight
-function miraloquetengo()
-{
-    for (let i = 0; i <flights.length; i++)
-    {
-        console.log(`El vuelo con origen\: ${flights[i]["from"]}, y destino\: ${flights[i]["to"]} tiene un coste de ${flights[i]["cost"]}€ y ${escala(flights[i]["scale"])}`)
+function miraloquetengo() {
+    for (let i = 0; i < flights.length; i++) {
+        console.log(
+            `El vuelo con origen\: ${flights[i]["from"]}, y destino\: ${
+                flights[i]["to"]
+            } tiene un coste de ${flights[i]["cost"]}€ y ${escala(
+                flights[i]["scale"]
+            )}`
+        );
     }
-};
+}
 
 //extra info given to user
 
 //averagecost
-function mediaCostes()
-{
-    let sumatorio=0
-for (let i = 0; i <flights.length; i++)
-    {
-        sumatorio+=flights[i]["cost"]
+function mediaCostes() {
+    let sumatorio = 0;
+    for (let i = 0; i < flights.length; i++) {
+        sumatorio += flights[i]["cost"];
     }
-    return (sumatorio)/(flights.length)
-};
+    return sumatorio / flights.length;
+}
 
 //scale counter
-function contadorEscalas()
-{
-    let numescalas=0
-    for (let i = 0; i <flights.length; i++)
-    {
-        if (flights[i]["scale"])
-        {
-            numescalas+=1
-        }   
+function contadorEscalas() {
+    let numescalas = 0;
+    for (let i = 0; i < flights.length; i++) {
+        if (flights[i]["scale"]) {
+            numescalas += 1;
+        }
     }
-    return numescalas
-};
+    return numescalas;
+}
 
 //last 5 flights destinies
-function last5D()
-{
-    let des5=[]
-for (let i = (flights.length-5); i <flights.length; i++)
-    {
-        des5.push(flights[i]["to"])
+function last5D() {
+    let des5 = [];
+    for (let i = flights.length - 5; i < flights.length; i++) {
+        des5.push(flights[i]["to"]);
     }
-    return des5.join(", ")
-};
+    return des5.join(", ");
+}
 
-//wrap up extra info 
-function extrainfo()
-{
-    console.log("El coste medio de los vuelos es "+mediaCostes()+".");
-    console.log("El número de vuelos que realizan escalas es "+contadorEscalas()+".");
-    console.log("Los últimos 5 vuelos del día tienen los siguientes destinos: "+last5D()+".");
-};
+//wrap up extra info
+function extrainfo() {
+    console.log("El coste medio de los vuelos es " + mediaCostes() + ".");
+    console.log(
+        "El número de vuelos que realizan escalas es " + contadorEscalas() + "."
+    );
+    console.log(
+        "Los últimos 5 vuelos del día tienen los siguientes destinos: " +
+            last5D() +
+            "."
+    );
+}
 
 //useradmin asker
-function useradmin()
-{
-    let respuestas=["USER","ADMIN"]
-    let tipoU=(prompt("Por favor, identifícate como USER o ADMIN")).toUpperCase()
-    if (tipoU==="ADMIN")
-    {
-        adminchosen()
+function useradmin() {
+    let respuestas = ["USER", "ADMIN"];
+    let tipoU = prompt(
+        "Por favor, identifícate como USER o ADMIN"
+    ).toUpperCase();
+    if (tipoU === "ADMIN") {
+        adminchosen();
     }
-    if (tipoU==="USER")
-    {
-        userchosen()
+    if (tipoU === "USER") {
+        userchosen();
+    } else {
+        alert("No es una palabra válida.");
+        useradmin();
     }
-    else
-    {
-        alert("No es una palabra válida.")
-        useradmin()
-    }
-};
+}
 
 //If User
 
 //funcion saca precios
-function sacaprecios()
-{
-    let listaprecios=[]
-    for (let i=0; i <flights.length; i++)
-        {listaprecios.push(flights[i]["cost"])}
+function sacaprecios() {
+    let listaprecios = [];
+    for (let i = 0; i < flights.length; i++) {
+        listaprecios.push(flights[i]["cost"]);
+    }
 
-    return listaprecios
+    return listaprecios;
 }
-function userchosen()
-{
-    let listaprecios=sacaprecios()
- let ordenuser=prompt("Introduce un precio para buscar un vuelo con ese coste. \nIntroduce caro o barato para ver el vuelo que corresponda a ello.\nIntroduce comprar para adquirir un vuelo.")
-        if (ordenuser==="caro")
-        {
-            let getMax = listaprecios.indexOf(Math.max(...listaprecios));
-            alert(`Vuelo con origen\: ${flights[getMax]["from"]}, y destino\: ${flights[getMax]["to"]} tiene un coste de ${flights[getMax]["cost"]}€ y ${escala(flights[getMax]["scale"])}. Su ID es ${flights[getMax]["id"]}.`)
-            console.log(`Vuelo con origen\: ${flights[getMax]["from"]}, y destino\: ${flights[getMax]["to"]} tiene un coste de ${flights[getMax]["cost"]}€ y ${escala(flights[getMax]["scale"])}. Su ID es ${flights[getMax]["id"]}.`)
-            userchosen()
+function userchosen() {
+    let listaprecios = sacaprecios();
+    let ordenuser = prompt(
+        "Introduce un precio para buscar un vuelo con ese coste. \nIntroduce caro o barato para ver el vuelo que corresponda a ello.\nIntroduce comprar para adquirir un vuelo."
+    );
+    if (ordenuser === "caro") {
+        let getMax = listaprecios.indexOf(Math.max(...listaprecios));
+        alert(
+            `Vuelo con origen\: ${flights[getMax]["from"]}, y destino\: ${
+                flights[getMax]["to"]
+            } tiene un coste de ${flights[getMax]["cost"]}€ y ${escala(
+                flights[getMax]["scale"]
+            )}. Su ID es ${flights[getMax]["id"]}.`
+        );
+        console.log(
+            `Vuelo con origen\: ${flights[getMax]["from"]}, y destino\: ${
+                flights[getMax]["to"]
+            } tiene un coste de ${flights[getMax]["cost"]}€ y ${escala(
+                flights[getMax]["scale"]
+            )}. Su ID es ${flights[getMax]["id"]}.`
+        );
+        userchosen();
+    } else if (ordenuser === "barato") {
+        let getMin = listaprecios.indexOf(Math.min(...listaprecios));
+        alert(
+            `Vuelo con origen\: ${flights[getMin]["from"]}, y destino\: ${
+                flights[getMin]["to"]
+            } tiene un coste de ${flights[getMin]["cost"]}€ y ${escala(
+                flights[getMin]["scale"]
+            )}. Su ID es ${flights[getMin]["id"]}.`
+        );
+        console.log(
+            `Vuelo con origen\: ${flights[getMin]["from"]}, y destino\: ${
+                flights[getMin]["to"]
+            } tiene un coste de ${flights[getMin]["cost"]}€ y ${escala(
+                flights[getMin]["scale"]
+            )}. Su ID es ${flights[getMin]["id"]}.`
+        );
+        userchosen();
+    } else if (ordenuser === "comprar") {
+        let compra = prompt(
+            "Introduce el número de ID del vuelo que deseas adquirir"
+        );
+        alert(`Gracias por su compra, vuelva pronto.`);
+    } else {
+        ordenuser = parseInt(ordenuser);
+        if (listaprecios.includes(ordenuser)) {
+            let getExact = listaprecios.indexOf(ordenuser);
+            alert(
+                `Vuelo con origen\: ${flights[getExact]["from"]}, y destino\: ${
+                    flights[getExact]["to"]
+                } tiene un coste de ${flights[getExact]["cost"]}€ y ${escala(
+                    flights[getExact]["scale"]
+                )}. Su ID es ${flights[getMax]["id"]}.`
+            );
+            console.log(
+                `Vuelo con origen\: ${flights[getExact]["from"]}, y destino\: ${
+                    flights[getExact]["to"]
+                } tiene un coste de ${flights[getExact]["cost"]}€ y ${escala(
+                    flights[getExact]["scale"]
+                )}. Su ID es ${flights[getMax]["id"]}.`
+            );
+            userchosen();
+        } else {
+            alert(
+                "No hay ningún vuelo con ese precio o la palabra introducida no es correcta.\nVuelve a intentarlo, anda."
+            );
+            userchosen();
         }
-        else if (ordenuser==="barato")
-        {
-            let getMin = listaprecios.indexOf(Math.min(...listaprecios));
-            alert(`Vuelo con origen\: ${flights[getMin]["from"]}, y destino\: ${flights[getMin]["to"]} tiene un coste de ${flights[getMin]["cost"]}€ y ${escala(flights[getMin]["scale"])}. Su ID es ${flights[getMin]["id"]}.`)
-            console.log(`Vuelo con origen\: ${flights[getMin]["from"]}, y destino\: ${flights[getMin]["to"]} tiene un coste de ${flights[getMin]["cost"]}€ y ${escala(flights[getMin]["scale"])}. Su ID es ${flights[getMin]["id"]}.`)
-            userchosen()
-        }
-        else if (ordenuser==="comprar")
-        {
-            let compra=prompt("Introduce el número de ID del vuelo que deseas adquirir")
-            alert(`Gracias por su compra, vuelva pronto.`)
-
-        }
-        else
-        {
-                ordenuser=parseInt(ordenuser)
-            if (listaprecios.includes(ordenuser))
-            {
-                let getExact=listaprecios.indexOf(ordenuser)
-                alert(`Vuelo con origen\: ${flights[getExact]["from"]}, y destino\: ${flights[getExact]["to"]} tiene un coste de ${flights[getExact]["cost"]}€ y ${escala(flights[getExact]["scale"])}. Su ID es ${flights[getMax]["id"]}.`)
-                console.log(`Vuelo con origen\: ${flights[getExact]["from"]}, y destino\: ${flights[getExact]["to"]} tiene un coste de ${flights[getExact]["cost"]}€ y ${escala(flights[getExact]["scale"])}. Su ID es ${flights[getMax]["id"]}.`)
-                userchosen()
-            }
-
-            else
-            {
-                alert("No hay ningún vuelo con ese precio o la palabra introducida no es correcta.\nVuelve a intentarlo, anda.")
-                userchosen()
-            }
-        }      
+    }
 }
 
 //If admin
 
-    //function create
-    function siatrue(scale)
-    {
-        if (scale==="si")
-            {
-                scale=true
-                return scale 
-            }
-            else 
-                {scale=false
-                    return scale}
+//function create
+function siatrue(scale) {
+    if (scale === "si") {
+        scale = true;
+        return scale;
+    } else {
+        scale = false;
+        return scale;
     }
-    
-    function createf()
-    {
-  if (flights.length < 16) {
-    let newFlightInput = prompt("Añade los datos del nuevo vuelo siguiendo este esquema: ID, destino, origen, coste, escala. Ejemplo: 13, Roma, La paz, 560, si").split(', ');
-    if (newFlightInput) {
-      const [id, to, from, price, scale] = newFlightInput;
-      let newFlightObject = ({id: parseInt(id),to,from,cost: parseInt(price),scale: siatrue(scale)});
-      flights.push(newFlightObject);
-      adminchosen()
-    }
-  } else {
-    alert("Hay 15 vuelos, no pueden crearse más");
-    adminchosen()
+}
 
-    }
-};
-
-
-
-    //function delete
-    function deleteflight()
-    {
-        if (flights.length>1)
-        {
-        let idToDel=parseInt(prompt("Introduce el id del vuelo que desea borrar."))
-
-          if (flights.find((id) => id.id ==idToDel)) 
-          {
-            flights=flights.filter((id) => id.id !== idToDel)
-            miraloquetengo()
-            adminchosen()
+function createf() {
+    if (flights.length < 16) {
+        let newFlightInput = prompt(
+            "Añade los datos del nuevo vuelo siguiendo este esquema: ID, destino, origen, coste, escala. Ejemplo: 13, Roma, La paz, 560, si"
+        ).split(", ");
+        if (newFlightInput) {
+            const [id, to, from, price, scale] = newFlightInput;
+            let newFlightObject = {
+                id: parseInt(id),
+                to,
+                from,
+                cost: parseInt(price),
+                scale: siatrue(scale),
+            };
+            flights.push(newFlightObject);
+            adminchosen();
         }
-        else
-        {
-            alert("No es un id válido.")
+    } else {
+        alert("Hay 15 vuelos, no pueden crearse más");
+        adminchosen();
+    }
+}
+
+//function delete
+function deleteflight() {
+    if (flights.length > 1) {
+        let idToDel = parseInt(
+            prompt("Introduce el id del vuelo que desea borrar.")
+        );
+
+        if (flights.find((id) => id.id == idToDel)) {
+            flights = flights.filter((id) => id.id !== idToDel);
+            miraloquetengo();
+            adminchosen();
+        } else {
+            alert("No es un id válido.");
             deleteflight();
-            }
         }
-        else{ alert("No quedan vuelos. Por favor, crea alguno o cierra el programa.")
-            adminchosen()}                    
-    };
-   
-         
-
-function adminchosen()
-{
-let adminAction=prompt("Hola de nuevo, Admin. Tus opciones son:\n1-Crear vuelos, con un máximo de 15.\n2-Borrar vuelos..\nEscribe crear o borrar para flights[getMax]ar la acción.")
-
-if (adminAction=="crear")
-{
-createf()
+    } else {
+        alert("No quedan vuelos. Por favor, crea alguno o cierra el programa.");
+        adminchosen();
+    }
 }
-else if (adminAction=="borrar")
-{
 
-deleteflight()
+function adminchosen() {
+    let adminAction = prompt(
+        "Hola de nuevo, Admin. Tus opciones son:\n1-Crear vuelos, con un máximo de 15.\n2-Borrar vuelos..\nEscribe crear o borrar para flights[getMax]ar la acción."
+    );
+
+    if (adminAction == "crear") {
+        createf();
+    } else if (adminAction == "borrar") {
+        deleteflight();
+    } else {
+        alert("Por favor, escribe una de las opciones correctamente.");
+        adminchosen();
+    }
 }
-else
-{
-    alert("Por favor, escribe una de las opciones correctamente.")
-    adminchosen()
-};
-};
 
 //general call function
-function ordenPrincipal()
-{
+function ordenPrincipal() {
     userwelcome();
     miraloquetengo();
     extrainfo();
     useradmin();
+}
 
-};
-
-ordenPrincipal()
-
-
+ordenPrincipal();
