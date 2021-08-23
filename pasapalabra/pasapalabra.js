cbf830225448f365a4677f43ba2bd5f00fb211d0
Juanma42 <juanmanueltabalv@gmail.com>
2021-08-23 12:22:13 +0200

    pequeñas correcciones

--- pasapalabra/pasapalabra.js
+++ pasapalabra/pasapalabra.js
@@ -1,315 +1,165 @@
 //funcion global
-function pasapalabra() {
+function pasapalabra()
+{
+
     // Elimina los diacríticos de un texto excepto si es una "ñ" (ES6) y pone en minusculas
 
-    function eliminarDiacriticosEs(texto) {
+    function eliminarDiacriticosEs(texto) 
+    {
+
         return texto
-            .normalize("NFD")
-            .replace(
-                /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
-                "$1"
-            )
-            .normalize()
-            .toLowerCase();
+               .normalize('NFD')
+               .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1")
+               .normalize().toLowerCase();
     }
 
     //asks for user and greets
 
-    function userwelcome() {
-        alert(
-            `Bienvenida sea la persona ${user}.\nTienes 90 segundos para responder a 27 preguntas.\nSi respondes correctamente, ganarás 10 puntos.\nSi respondes incorrectamente, perderás 5 puntos.\nLas preguntas sin contestar ni suman ni restan.\nSi quieres saltarte una pregunta, responde pasapalabra.\n¡Suerte en el concurso!.`
-        );
-    }
+    function userwelcome()
+    {
+        alert(`Bienvenida sea la persona ${user}.\nTienes 90 segundos para responder a 27 preguntas.\nSi respondes correctamente, ganarás 10 puntos.\nSi respondes incorrectamente, perderás 5 puntos.\nLas preguntas sin contestar ni suman ni restan.\nSi quieres saltarte una pregunta, responde pasapalabra.\n¡Suerte en el concurso!.`)
+    };
 
     //get the status of all questions
 
-    function statusCheck() {
-        let listaStatus = [];
-        for (let i = 0; i < questions.length; i++) {
-            listaStatus.push(questions[i].status);
-        }
+    function statusCheck()
+    {
+        let listaStatus=[]
+        for (let i=0; i <questions.length; i++)
+            {listaStatus.push(questions[i].status)}
 
-        return listaStatus;
+        return listaStatus
     }
 
     //Starts the next unanswered question
 
-    function nextQuestion() {
-        for (var i = 0; i < questions.length; i++) {
-            if (questions[i].status === 0) {
-                let userAnswer = eliminarDiacriticosEs(
-                    prompt(questions[i].question)
-                );
-                if (userAnswer === "pasapalabra") {
-                    alert("Siguiente pregunta.");
-                } else if (userAnswer === questions[i].answer) {
-                    alert("¡Correcto!");
-                    questions[i].status = 1;
-                    userPoints += 10;
-                    if (statusCheck().every((s) => s === 1)) {
-                        clearTimeout(temporizador);
-                        finDelJuego();
+    function nextQuestion()
+    {
+        for (var i = 0; i <questions.length; i++)
+        {                       
+            if (questions[i].status===0)
+                {
+                    let userAnswer=eliminarDiacriticosEs(prompt(questions[i].question))
+                    if (userAnswer==="pasapalabra")
+                    {
+                        alert("Siguiente pregunta.")
                     }
-                } else {
-                    alert("Incorrecto.");
-                    questions[i].status = 1;
-                    userPoints -= 5;
-                    if (statusCheck().every((s) => s === 1)) {
-                        clearTimeout(temporizador);
-                        finDelJuego();
+                    else if (userAnswer===questions[i].answer)
+                    {
+                        alert("¡Correcto!")
+                        questions[i].status=1
+                        userPoints+=10
+                        if (statusCheck().every(s => s === 1))
+                        {
+                            clearTimeout(temporizador)
+                            finDelJuego()
+                        }
                     }
+                    else
+                    {
+                        alert("Incorrecto.")
+                        questions[i].status=1
+                        userPoints-=5
+                        if (statusCheck().every(s => s === 1))
+                        {
+                            clearTimeout(temporizador)
+                            finDelJuego()
+                        }
+                    }
+
                 }
-            }
+
         }
-        nextQuestion();
+        nextQuestion()
+
     }
 
-    //game ending
-    function finDelJuego() {
-        alert(
-            `¡Se ha terminado! ${user}, tu puntuación ha sido ${userPoints}. Mira en la consola cómo has quedado en el ranking.`
-        );
+   //game ending
+   function finDelJuego()
+   {
+        alert(`¡Se ha terminado! ${user}, tu puntuación ha sido ${userPoints}. Mira en la consola cómo has quedado en el ranking.`)
+        
+        puntuaciones.push({"nombre":user,"puntuacion":userPoints})
+        puntuaciones.sort((a, b) =>  b.puntuacion - a.puntuacion);
+        console.table(puntuaciones)
+        let vicio=confirm("¿Quieres jugar otra vez?")
+        if (vicio)
+        {location.reload()}
+        else {stop()}
+   }
 
-        puntuaciones.push({ nombre: user, puntuacion: userPoints });
-        puntuaciones.sort((a, b) => b.puntuacion - a.puntuacion);
-        console.table(puntuaciones);
-        let vicio = confirm("¿Quieres jugar otra vez?");
-        if (vicio) {
-            location.reload();
-        } else {
-            stop();
-        }
-    }
+
 
     const questions = [
-        {
-            letter: "a",
-            answer: "aragorn",
-            status: 0,
-            question:
-                "CON LA A. Legítimo Rey de Gondor en 'El señor de los anillos'",
-        },
 
-        {
-            letter: "b",
-            answer: "bastet",
-            status: 0,
-            question:
-                "CON LA B. Diosa egipcia de la protección, amor y armonía. Su cabeza tiene forma de gato",
-        },
+        { letter: "a", answer: "aragorn", status: 0, question: "CON LA A. Legítimo Rey de Gondor en 'El señor de los anillos'"},
 
-        {
-            letter: "c",
-            answer: "castamere",
-            status: 0,
-            question:
-                "CON LA C. De las novelas 'Cancion de hielo y fuego', 'Las lluvias de ...'",
-        },
+        { letter: "b", answer: "bastet", status: 0, question: "CON LA B. Diosa egipcia de la protección, amor y armonía. Su cabeza tiene forma de gato"},
 
-        {
-            letter: "d",
-            answer: "dinosaurio",
-            status: 0,
-            question:
-                "CON LA D. Tipo de animal grande y extinto que asombró a la generación de los 90",
-        },
+        { letter: "c", answer: "castamere", status: 0, question: "CON LA C. De las novelas 'Cancion de hielo y fuego', 'Las lluvias de ...'"},
 
-        {
-            letter: "e",
-            answer: "esperanto",
-            status: 0,
-            question:
-                "CON LA E. Idioma universal artificial creado por el polaco L. L. Zamenhof.",
-        },
+        { letter: "d", answer: "dinosaurio", status: 0, question: "CON LA D. Tipo de animal grande y extinto que asombró a la generación de los 90"},
 
-        {
-            letter: "f",
-            answer: "fantasia",
-            status: 0,
-            question:
-                "CON LA F. Genero literario donde aparecen caballeros, dragones y magia.",
-        },
+        { letter: "e", answer: "esperanto", status: 0, question: "CON LA E. Idioma universal artificial creado por el polaco L. L. Zamenhof."},
 
-        {
-            letter: "g",
-            answer: "galaxias",
-            status: 0,
-            question:
-                "CON LA G. Título original en España de aclamada película de ciencia ficción. 'La guerra de las ...'",
-        },
+        { letter: "f", answer: "fantasia", status: 0, question: "CON LA F. Genero literario donde aparecen caballeros, dragones y magia."},
 
-        {
-            letter: "h",
-            answer: "hajime",
-            status: 0,
-            question:
-                "CON LA H. Palabra japonesa utilizada para comenzar en artes marciales como Karate o Judo.",
-        },
+        { letter: "g", answer: "galaxias", status: 0, question: "CON LA G. Título original en España de aclamada película de ciencia ficción. 'La guerra de las ...'"},
 
-        {
-            letter: "i",
-            answer: "isengard",
-            status: 0,
-            question:
-                "CON LA I. Pais-Ciudad controlada por Saruman en 'El señor de los anillos'",
-        },
+        { letter: "h", answer: "hajime", status: 0, question: "CON LA H. Palabra japonesa utilizada para comenzar en artes marciales como Karate o Judo."},
 
-        {
-            letter: "j",
-            answer: "juanma",
-            status: 0,
-            question:
-                "CON LA J. Nombre abreviado del creador de este programa.",
-        },
+        { letter: "i", answer: "isengard", status: 0, question: "CON LA I. Pais-Ciudad controlada por Saruman en 'El señor de los anillos'"},
 
-        {
-            letter: "k",
-            answer: "katana",
-            status: 0,
-            question: "CON LA K. Espada japonesa portada por los samurais.",
-        },
+        { letter: "j", answer: "juanma", status: 0, question: "CON LA J. Nombre abreviado del creador de este programa."},
 
-        {
-            letter: "l",
-            answer: "leprechaun",
-            status: 0,
-            question:
-                "CON LA L. Duende de la mitología irlandesa que espera al final del arcoiris.",
-        },
+        { letter: "k", answer: "katana", status: 0, question: "CON LA K. Espada japonesa portada por los samurais."},
 
-        {
-            letter: "m",
-            answer: "mascaron",
-            status: 0,
-            question:
-                "CON LA M. Figura decorativa tallada en madera situada en la proa de antiguos buques.",
-        },
+        { letter: "l", answer: "leprechaun", status: 0, question: "CON LA L. Duende de la mitología irlandesa que espera al final del arcoiris."},
 
-        {
-            letter: "n",
-            answer: "nigromante",
-            status: 0,
-            question:
-                "CON LA N. Mago especializado en magia negra y resucitar muertos.",
-        },
+        { letter: "m", answer: "mascaron", status: 0, question: "CON LA M. Figura decorativa tallada en madera situada en la proa de antiguos buques."},
 
-        {
-            letter: "ñ",
-            answer: "ñu",
-            status: 0,
-            question: "CON LA Ñ. Antílope sudafricano.",
-        },
+        { letter: "n", answer: "nigromante", status: 0, question: "CON LA N. Mago especializado en magia negra y resucitar muertos."},
 
-        {
-            letter: "o",
-            answer: "orco",
-            status: 0,
-            question:
-                "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien",
-        },
+        { letter: "ñ", answer: "ñu", status: 0, question: "CON LA Ñ. Antílope sudafricano."},
 
-        {
-            letter: "p",
-            answer: "protoss",
-            status: 0,
-            question:
-                "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft",
-        },
+        { letter: "o", answer: "orco", status: 0, question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien"},
 
-        {
-            letter: "q",
-            answer: "queliceros",
-            status: 0,
-            question:
-                "CON LA Q. Piezas bucales presentes en un subfilo de artrópodos como los arácnidos.",
-        },
+        { letter: "p", answer: "protoss", status: 0, question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft"},
 
-        {
-            letter: "r",
-            answer: "rivendel",
-            status: 0,
-            question:
-                "CON LA R. Ciudad Elfica gobernada por Elrond en 'El señor de los anillos'",
-        },
+        { letter: "q", answer: "queliceros", status: 0, question: "CON LA Q. Piezas bucales presentes en un subfilo de artrópodos como los arácnidos."},
 
-        {
-            letter: "s",
-            answer: "stackoverflow",
-            status: 0,
-            question:
-                "CON LA S. Comunidad salvadora de todo desarrollador informático",
-        },
+        { letter: "r", answer: "rivendel", status: 0, question: "CON LA R. Ciudad Elfica gobernada por Elrond en 'El señor de los anillos'"},
 
-        {
-            letter: "t",
-            answer: "terminator",
-            status: 0,
-            question:
-                "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984",
-        },
+        { letter: "s", answer: "stackoverflow", status: 0, question: "CON LA S. Comunidad salvadora de todo desarrollador informático"},
 
-        {
-            letter: "u",
-            answer: "ukelele",
-            status: 0,
-            question:
-                "CON LA U. Instrumento de cuerda similar a la guitarra pero más pequeño.",
-        },
+        { letter: "t", answer: "terminator", status: 0, question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984"},
 
-        {
-            letter: "v",
-            answer: "vikingos",
-            status: 0,
-            question:
-                "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa",
-        },
+        { letter: "u", answer: "ukelele", status: 0, question: "CON LA U. Instrumento de cuerda similar a la guitarra pero más pequeño."},
 
-        {
-            letter: "w",
-            answer: "wanda",
-            status: 0,
-            question:
-                "CON LA W. Nombre de la bruja escarlata en el universo Marvel",
-        },
+        { letter: "v", answer: "vikingos", status: 0, question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa"},
 
-        {
-            letter: "x",
-            answer: "xilofono",
-            status: 0,
-            question:
-                "CON LA X. Instrumento musical de percusión con láminas afinadas cada una a un tono específico.",
-        },
+        { letter: "w", answer: "wanda", status: 0, question: "CON LA W. Nombre de la bruja escarlata en el universo Marvel"},
 
-        {
-            letter: "y",
-            answer: "Yeti",
-            status: 0,
-            question:
-                "CONTIENE LA Y. Abobinable hombre de las nieves, críptido interpretado como un simio gigante del himalaya.",
-        },
+        { letter: "x", answer: "xilofono", status: 0, question: "CON LA X. Instrumento musical de percusión con láminas afinadas cada una a un tono específico."},
 
-        {
-            letter: "z",
-            answer: "zion",
-            status: 0,
-            question:
-                "CON LA Z. Vulpes vulpes, mamífero de la familia cánidos, depredador de conejos y gallinas entre otros. ",
-        },
-    ];
+        { letter: "y", answer: "yeti", status: 0, question: "CONTIENE LA Y. Abobinable hombre de las nieves, críptido interpretado como un simio gigante del himalaya."},
 
-    //Orden principal
-    let userPoints = 0;
-    let user = prompt("¿Cuál es tu nombre, concursante?");
-    userwelcome();
-    let tiempo = 90;
-    let puntuaciones = [
-        { nombre: "GamerGranny", puntuacion: 220 },
-        { nombre: "AdolfoNumber1", puntuacion: 170 },
-        { nombre: "ASS", puntuacion: 69 },
-        { nombre: "Güiner", puntuacion: 1 },
-        { nombre: "LaRespuesta", puntuacion: 42 },
-    ];
-    let temporizador = setTimeout(finDelJuego, 90000);
-    nextQuestion();
+        { letter: "z", answer: "zorro", status: 0, question: "CON LA Z. Vulpes vulpes, mamífero de la familia cánidos, depredador de conejos y gallinas entre otros. "}, ]
+
+
+        //Orden principal
+        let userPoints=0
+        let user=prompt("¿Cuál es tu nombre, concursante?");
+        if (user==="" || user===null){user="Anónimo"}
+        userwelcome()
+        let tiempo = 90
+        let puntuaciones=[{nombre:"GamerGranny",puntuacion:220},
+                            {nombre:"AdolfoNumber1",puntuacion:170},
+                            {nombre:"ASS",puntuacion:69},
+                            {nombre:"Güiner",puntuacion:1},
+                            {nombre:"LaRespuesta",puntuacion:42}]
+        let temporizador=setTimeout(finDelJuego,90000)
+        nextQuestion()
+
 }
-pasapalabra();
+pasapalabra()
